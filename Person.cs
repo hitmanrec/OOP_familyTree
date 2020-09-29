5fada083a52bb60a7b420c864389c189d60dab8f
hitmanrec <28543962+hitmanrec@users.noreply.github.com>
2020-09-29 19:17:01 +0500

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOP_familyTree
+{
+    public class Person
+    {
+        public String name, sex;
+        public int age;
+        public List<Person> childrens;
+        public Person married, mother, father;
+
+        public Person(string _name, string _sex, int _age)
+        {
+            name = _name;
+            if(_sex != "Male" && _sex != "Female")
+                throw new ArgumentException("Person's sex has to be Male or Female!");
+            sex = _sex;
+            if(_age <= 0 || _age > 150)
+                throw new ArgumentException("Person's age cannot be less than zero or greater than 150 years (people can't live for so long nowadays)!");
+            age = _age;
+            childrens = new List<Person>();
+        }
+
+        public void setMarriage(Person other)
+        {
+            if(this.married != null || other.married != null)
+                throw new ArgumentException("Person already married!");
+            if(this == other)
+                throw new ArgumentException("Person can't marry himself!");
+            else
+            {
+                this.married = other;
+                other.married = this;
+            }
+        }
+        public Person divorce()
+        {
+            if(this.married == null)
+                throw new Exception("Person not married!");
+            var other = this.married;
+            other.married = null;
+            this.married = null;
+            return other;
+        }
+        public void setChildrens(List<Person> babies, Person secondParent = null)
+        {
+            if (this.sex == secondParent.sex)
+                throw new ArgumentException("Parents are have the same sex! This class accepts only heterosexual parents (want to represent LGBT? find another way).");
+            foreach (var baby in babies)
+            {
+                if(baby.age >= this.age)
+                    throw new Exception("Childrens cannot be older than their parents!");
+                if (baby == this || baby == secondParent)
+                    throw new ArgumentException("Person can't be children of himself!");
+                if (this.sex == "Male")
+                {
+                    baby.father = this;
+                    if (secondParent != null)
+                        baby.mother = secondParent;
+                }
+                else
+                {
+                    baby.mother = this;
+                    if (secondParent != null)
+                        baby.father = secondParent;
+                }
+                this.childrens.Add(baby);
+            }
+        }
+        public void printParents()
+        {
+            if (this.father != null)
+                Console.WriteLine(father.name);
+            if (this.mother != null)
+                Console.WriteLine(mother.name);
+        }
+        public void printSiblings()
+        {
+            var sibs = this.getSiblings();
+            if(sibs.Count > 0)
+            foreach(var sib in sibs)
+                Console.WriteLine(sib.name);
+            else
+                Console.WriteLine("No siblings :(");
+        }
+        private List<Person> getSiblings()
+        {
+            List<Person> siblings = new List<Person>();
+            if (this.father != null)
+                foreach (var sibling in this.father.childrens)
+                    if (sibling != this)
+                        siblings.Add(sibling);
+            if (this.mother != null)
+                foreach (var sibling in this.mother.childrens)
+                    if (sibling != this)
+                        siblings.Add(sibling);
+            List<Person> _siblings = siblings.Distinct().ToList();
+            return _siblings;
+        }
+        public void printUnclesAndAunts()
+        {
+            var relatives = this.getUnclesAndAunts();
+            if(relatives.Count > 0)
+                foreach(var relative in relatives)
+                    Console.WriteLine(relative.name);
+            else
+                Console.WriteLine("No aunts or uncles :(");
+        }
+
+        private List<Person> getUnclesAndAunts()
+        {
+            List<Person> relatives = new List<Person>();
+            if (this.father != null)
+            {
+                var sibF = this.father.getSiblings();
+                if(sibF.Count > 0)
+                foreach (var relative in sibF)
+                        relatives.Add(relative);
+            }
+            if (this.mother != null)
+            {
+                var sibM = this.mother.getSiblings();
+                if(sibM.Count > 0)
+                foreach (var relative in sibM)
+                    relatives.Add(relative);
+            }
+            List<Person> _relatives = relatives.Distinct().ToList();
+            return _relatives;
+        }
+
+        private List<Person> getCousins()
+        {
+            List<Person> cousins = new List<Person>();
+
+            var relatives = this.getUnclesAndAunts();
+            if(relatives.Count > 0)
+                foreach(var relative in relatives)
+                    foreach(var baby in relative.childrens)
+                        cousins.Add(baby);
+
+            List<Person> _cousins = cousins.Distinct().ToList();
+            return _cousins;
+        }
+
+        public void printCousins()
+        {
+            var cousins = this.getCousins();
+            if(cousins.Count > 0)
+            foreach(var cousin in cousins)
+            {
+                Console.WriteLine(cousin.name);
+            }
+            else
+            {
+                Console.WriteLine("No cousins :(");
+            }
+        }
+
+        public void printParentsInLaw()
+        {
+            if (this.married == null)
+                Console.WriteLine("Person not married.");
+            else
+            {
+                if (this.married.father != null)
+                    Console.WriteLine(this.married.father.name);
+                if (this.married.mother != null)
+                    Console.WriteLine(this.married.mother.name);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+namespace OOP_familyTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> family = new List<Person>();
+            family.Add(new Person("Jan Simon", "Female", 80));//0
+            family.Add(new Person("John Hamilton", "Male", 84));
+            family.Add(new Person("Abby Hamilton", "Female", 58));//2
+            family.Add(new Person("Steve Smith", "Male", 64));
+            family.Add(new Person("John Hamilton jr.", "Male", 60));//4
+            family.Add(new Person("Gabi Julian", "Female", 58));
+            family.Add(new Person("Barb Hamilton", "Female", 55));//6
+            family.Add(new Person("Dale Celebs", "Male", 58));
+            family.Add(new Person("Fae Smith", "Female", 36));//8
+            family.Add(new Person("Cami Smith", "Female", 32));
+            family.Add(new Person("Dale Marx", "Male", 38));//10
+            family.Add(new Person("Meg Hamilton", "Female", 39));
+            family.Add(new Person("Pam Celebs", "Female", 30));//12
+            family.Add(new Person("Mark Grant", "Male", 32));
+            family.Add(new Person("Alex Jones", "Female", 33));//14
+            family.Add(new Person("Mike Celebs", "Male", 33));
+            family.Add(new Person("Angie Grant", "Female", 10));//16
+            family.Add(new Person("Mary Celebs", "Female", 12));
+
+            family[0].setMarriage(family[1]);
+            family[0].setChildrens(new List<Person> { family[2], family[4], family[6] }, family[1]);
+
+            family[2].setMarriage(family[3]);
+            family[4].setMarriage(family[5]);
+            family[6].setMarriage(family[7]);
+
+            family[2].setChildrens(new List<Person> { family[8], family[9] }, family[3]);
+            family[4].setChildrens(new List<Person> { family[11] }, family[5]);
+            family[6].setChildrens(new List<Person> { family[12], family[15] }, family[7]);
+
+            family[9].setMarriage(family[10]);
+            family[12].setMarriage(family[13]);
+            family[14].setMarriage(family[15]);
+
+            family[12].setChildrens(new List<Person> { family[16] }, family[13]);
+            family[15].setChildrens(new List<Person> { family[17] }, family[14]);
+
+            Console.WriteLine("Cousins of " + family[12].name);
+            family[12].printCousins();
+            Console.WriteLine("Parents of " + family[12].name);
+            family[12].printParents();
+            Console.WriteLine("Uncles and aunts of " + family[12].name);
+            family[12].printUnclesAndAunts();
+            Console.WriteLine("Parents in law of " + family[13].name);
+            family[13].printParentsInLaw();
+        }
+    }
+}
